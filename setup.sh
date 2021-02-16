cb6a5f9856ba17b06f9629a5480e9aa6046f0e37
Maxim Makhun <maxim.makhun@gmail.com>
2021-02-15 22:05:31 -0800

    Add applications installer.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 CURRENT_PATH="$(pwd)"
 DOTFILES=(.shell .aliases .bash_profile .bashrc .common .macos .nanorc .zprofile .zshrc)
 
@@ -12,3 +15,42 @@ done
 # Start activity maintainer deamon
 cd ~/.shell/daemons/activity_maintainer/
 sudo ./start.sh
+
+APPLICATIONS=( \
+alfred \
+visual-studio-code \
+skype \
+sourcetree \
+spotify \
+virtualbox \
+pycharm-ce \
+wireshark \
+vlc \
+openoffice \
+telegram \
+rescuetime \
+sublime-text \
+powershell \
+viber \
+airserver \
+android-file-transfer \
+macdown \
+gpac \
+selfcontrol \
+machoview \
+android-studio \
+angry-ip-scanner \
+hex-fiend \
+cmake \
+db-browser-for-sqlite \
+iterm2 \
+grandperspective \
+hopper-debugger-server \
+ios-app-signer \
+google-chrome \
+)
+
+for APPLICATION in ${APPLICATIONS[*]}
+do
+brew reinstall ${APPLICATION}
+done
