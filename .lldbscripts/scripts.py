f26830f10379cce9381bd82a7f2b3ac22269bb52
Maxim Makhun <maxim.makhun@gmail.com>
2021-10-01 14:49:21 -0700

    Update.

--- .lldbscripts/scripts.py
+++ .lldbscripts/scripts.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 def __lldb_init_module(debugger, dict):
-    help = "Available LLDB commands:\n\t- json JSON_STRING or JSON_DATA"
+    help = "Available LLDB commands:\n\t- json JSON/Data\n\t- validate UITableView/UICollectionView"
 
     print(help)
 
     json_swift_command = """
+        let input = %1
         let output: Any
         do {
             if let input = input as? String,
@@ -31,3 +32,40 @@ def __lldb_init_module(debugger, dict):
 
     json_lldb_command = "command regex json 's/(.+)/expr {} /'".format(json_swift_command)
     debugger.HandleCommand(json_lldb_command)
+
+    validate_swift_command = """
+        extension UITableView {
+            
+            @objc open override func value(forUndefinedKey key: String) -> Any? {
+                return nil
+            }
+        }
+
+        extension UICollectionView {
+            
+            @objc open override func value(forUndefinedKey key: String) -> Any? {
+                return nil
+            }
+        }
+
+        let input = %1
+
+        if let tableView = input as? UITableView {
+            print("UITableViewDelegate: \((tableView.delegate != nil) ? "Set" : "Not set").")
+            print("UITableViewDataSource: \((tableView.dataSource != nil) ? "Set" : "Not set").")
+            
+            print("Registered cell classes: \(tableView.value(forKey: "_cellClassDict") ?? "Not set").")
+            print("Registered nibs: \(tableView.value(forKey: "_nibMap") ?? "Not set").")
+        } else if let collectionView = input as? UICollectionView {
+            print("UITableViewDelegate: \((collectionView.delegate != nil) ? "Set" : "Not set").")
+            print("UITableViewDataSource: \((collectionView.dataSource != nil) ? "Set" : "Not set").")
+            
+            print("Registered cell classes: \(collectionView.value(forKey: "_cellClassDict") ?? "Not set").")
+            print("Registered nibs: \(collectionView.value(forKey: "_nibMap") ?? "Not set").")
+        } else {
+            print("Unsupported operation.")
+        }
+    """
+
+    validate_lldb_command = "command regex validate 's/(.+)/expr {} /'".format(validate_swift_command)
+    debugger.HandleCommand(validate_lldb_command)
