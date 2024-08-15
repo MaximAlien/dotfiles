8d285ab70734c490098554cc2ed260e3510da8e7
Maxim Makhun <maxim.makhun@gmail.com>
2024-08-15 12:28:07 -0700

    Update.

--- .shell/git_branch_name.swift
+++ .shell/git_branch_name.swift
@@ -17,9 +17,10 @@ guard CommandLine.arguments.count == 2 else {
 let gitBranchName = CommandLine.arguments[1]
     .lowercased()
     .filter {
-        $0.isLetter || $0.isWhitespace
+        $0.isLetter || $0.isWhitespace || $0.isNumber
     }
     .condensed
+    .trimmingCharacters(in: .whitespacesAndNewlines)
     .replacingOccurrences(of: " ", with: "-")
 
 print(gitBranchName)
